8bdb0350eba7935aae98d5c1d429213ebd765760
Russell2259 <russell2259@tuta.io>
2023-04-11 05:29:34 -0600

    organize files, update some stuff

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static(path.join(__dirname, '/')));
-app.listen(8080);
+const port = process.env.port || 8080;
+
+app.use(express.static(path.join(__dirname, '/static')));
+
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, './static/', '404.html'));
+});
+
+const server = app.listen(port, () => {
+    console.log(`Polaris is running on port ${server.address().port}, using nodejs ${process.version}`)
+});
