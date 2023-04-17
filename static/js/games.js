30eb668e0f149f4b56d024062498f7a904e73249
Cobalt-60 <95923271+plastics-eater@users.noreply.github.com>
2023-04-16 20:04:14 -0400

    Update games.js

--- static/js/games.js
+++ static/js/games.js
@@ -1,1 +1,29 @@
+const spinner = document.getElementById('spinner-container');
+spinner.innerHTML = '<div class="spinner"></div>';
 
+fetch('/assets/JSON/games.json')
+    .then((response) => response.json())
+    .then((data) => {
+        const container = document.getElementById('card-container');
+        for (const game of data) {
+            const card = document.createElement('div');
+            card.classList.add('card');
+            const link = document.createElement('a');
+            const image = document.createElement('img');
+            image.classList.add('cardimage');
+            image.src = game.image;
+            const name = document.createElement('p');
+            name.classList.add('cardname');
+            name.textContent = game.name;
+            link.appendChild(image);
+            link.appendChild(name);
+            card.appendChild(link);
+            container.appendChild(card);
+        }
+
+        spinner.remove();
+    })
+    .catch((error) => {
+        spinner.remove();
+        console.error(error);
+    });
